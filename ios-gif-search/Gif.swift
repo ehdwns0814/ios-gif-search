a159c8def51a9429c44373cf77eb845fa9c009ec
DongJun96 <djlee1201@naver.com>
2024-03-07 13:21:03 +0900

    feat: 이미지 수신 및 Codingkey 구현

---
+++ ios-gif-search/Gif.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct GifArray: Decodable {
+    var gifs: [Gif]
+    enum CodingKeys: String, CodingKey {
+        case gifs = "data"
+    }
+}
+
+struct Gif: Decodable {
+    var gifSources: GifImages
+    enum CodingKeys: String, CodingKey {
+        case gifSources = "images"
+    }
+    
+    func getGifURL() -> String{
+        return gifSources.original.url
+    }
+}
+
+struct GifImages: Decodable {
+    var original: original
+    enum CodingKeys: String, CodingKey {
+        case original = "original"
+    }
+}
+
+struct original: Decodable {
+    var url: String
+}
