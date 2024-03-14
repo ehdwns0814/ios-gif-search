6fd071f6ec037f35530f503090a0ab0fc7165ecc
DongJun96 <djlee1201@naver.com>
2024-03-14 16:20:40 +0900

    feat: collectionViewCell 구현

---
+++ ios-gif-search/Cells/GifCell.swift
@@ -0,0 +1,38 @@
+//
+//  GifCell.swift
+//  ios-gif-search
+//
+//  Created by 동준 on 3/2/24.
+//
+
+import UIKit
+
+class GifCell: UICollectionViewCell {
+    
+    static let identifier = "GifCell"
+    var gif: Gif?
+    
+    lazy var gifView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if gif != nil {
+            let gifURL = gif!.getGifURL()
+            gifView.image = UIImage.gif(url: gifURL)
+            gifView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+            addSubview(gifView)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
