ac2f0d0b167188f35427da485dd91db0fcc3a3cc
DongJun96 <djlee1201@naver.com>
2024-03-17 14:05:52 +0900

    chore: 파일 삭제 및 이동

--- ios-gif-search/Cells/GifCell.swift
+++ ios-gif-search/Cells/GifCell.swift
@@ -15,18 +15,19 @@ class GifCell: UICollectionViewCell {
     lazy var gifView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFit
+        view.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
         return view
     }()
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        if gif != nil {
-            let gifURL = gif!.getGifURL()
-            gifView.image = UIImage.gif(url: gifURL)
-            gifView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
-            addSubview(gifView)
-        }
-    }
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        if gif != nil {
+//            let gifURL = gif!.getGifURL()
+//            gifView.image = UIImage.gif(url: gifURL)
+//            gifView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+//            addSubview(gifView)
+//        }
+//    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
