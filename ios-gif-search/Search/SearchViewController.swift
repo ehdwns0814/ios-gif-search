7ad39984120516d14fb31752068352f7c342bd59
DongJun96 <djlee1201@naver.com>
2024-03-29 19:07:15 +0900

    feat: 데이터 바인딩 구현
    - network: viewModel로 이동
    - setupData(): 데이터를 저장소에 저장
    - setupBinding(): 저장된 데이터를 collectionView에 바인딩

--- ios-gif-search/Search/SearchViewController.swift
+++ ios-gif-search/Search/SearchViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 class SearchViewController: UIViewController {
     
     let homeView = HomeView()
-    let network = GifNetwork(type: .search)
-
-    var viewModel = GifListViewModel()
+    var viewModel = SearchViewModel()
 
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -29,24 +27,22 @@ class SearchViewController: UIViewController {
         
         return collectionView
     }()
-        
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        
+        setupData()
         setupViews()
         setupLayouts()
-        
-        viewModel.gifModels.bind { [weak self] _ in
-            DispatchQueue.main.async {
-                self?.collectionView.reloadData()
-            }
-        }
-
     }
     
     func setupViews() {
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+//        setupBinding()
         
         homeView.searchBar.searchTextField.delegate = self
         homeView.searchBar.searchTextField.placeholder = "Search Gifs"
@@ -74,27 +70,29 @@ class SearchViewController: UIViewController {
         ])
     }
     
-    func fetchGifs(for searchText: String) {
-        guard let url = network.searchUrlBuilder(searchTerm: searchText, type: "gifs") else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let err = error {
-                print("Error fetching from Giphy: ", err.localizedDescription)
-            }
-            guard let data = data else { return }
-                    
-            do {
-                let gifs = try JSONDecoder().decode([Gifs].self, from: data)
-                self.viewModel.gifModels.value = gifs.compactMap({
-                    
-                })
-            } catch {
-                
-            }
-        }.resume()
+    // MARK: - Helpers
+    private func setupData() {
+        viewModel.fetchData(searchType: .gif, searchMenu: .trending)
     }
     
+    private func setupBinding(){
+        viewModel.storage.bind { [weak self] _ in
+            guard let self = self else { return }
+            self.collectionView.reloadData()
+        }
+        
+        //에러
+        let message = "setupBinding - Binding error"
+        viewModel.errorMessage = Observable(message)
+        
+        viewModel.error.bind { isSuccess in
+            if isSuccess {
+                print("DEBUG: success")
+            } else {
+                print("DEBUG: \(self.viewModel.errorMessage)")
+            }
+        }
+    }
 }
 
 extension SearchViewController: UICollectionViewDelegate {
@@ -103,13 +101,17 @@ extension SearchViewController: UICollectionViewDelegate {
 
 extension SearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(viewModel.numberOfItems())
-        return viewModel.numberOfItems()
+        print(viewModel.storage.value.count)
+        return viewModel.storage.value.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GifCell.identifier, for: indexPath) as! GifCell
-//        cell.gif = viewModel.gifAtIndex(indexPath.row)
+        
+        viewModel.network.configure(gif: viewModel.storage.value[indexPath.row]) { image in
+            guard let image = image else { return }
+            cell.configure(image)
+        }
         return cell
     } 
 }
@@ -118,7 +120,7 @@ extension SearchViewController: UISearchTextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         if textField.text != nil {
-            fetchGifs(for: textField.text!)
+            
         }
         return true
     }
