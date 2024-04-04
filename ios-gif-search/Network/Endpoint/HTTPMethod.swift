bf705ff0f398b37487f9acca91d19e3724d13ef5
DongJun96 <djlee1201@naver.com>
2024-04-04 11:23:09 +0900

    feat: httpMethod enum 구현

---
+++ ios-gif-search/Network/Endpoint/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  ios-gif-search
+//
+//  Created by 동준 on 4/2/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
