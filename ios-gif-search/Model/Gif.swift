0c520ceb1e7ffeab1fe5872bb146a4365e92b72f
DongJun96 <djlee1201@naver.com>
2024-04-07 12:16:07 +0900

    feat: 모델 구현 및 수정

--- ios-gif-search/Model/Gif.swift
+++ ios-gif-search/Model/Gif.swift
@@ -6,31 +6,15 @@
 //
 import Foundation
 
-struct GifArray: Decodable {
+struct Gif {
+    let type: GifType
+    let identifier: String
+    let user: User?
+    let mediaResource: MediaResource
+    let title: String
+}
+
+
+struct GifBundle {
     var gifs: [Gif]
-    enum CodingKeys: String, CodingKey {
-        case gifs = "data"
-    }
-}
-
-struct Gif: Decodable {
-    var gifSources: GifImages
-    enum CodingKeys: String, CodingKey {
-        case gifSources = "images"
-    }
-    
-    var gifURL: String {
-        return gifSources.original.url
-    }
-}
-
-struct GifImages: Decodable {
-    var original: original
-    enum CodingKeys: String, CodingKey {
-        case original = "original"
-    }
-}
-
-struct original: Decodable {
-    var url: String
 }
---
+++ ios-gif-search/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ios-gif-search
+//
+//  Created by 동준 on 4/4/24.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let profileURL: String
+    let displayName: String
+}
